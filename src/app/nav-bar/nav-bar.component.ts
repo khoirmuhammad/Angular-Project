765985520a8ae93b419e2a0131fdc9a4bb302fc3
Muhammad Khoirudin <mk.muhammadkhoirudin@gmail.com>
2022-10-10 14:48:31 +0700

    Login Logout Functionality and install ngx bootstrap and font awesome utilization

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AlertifyService } from '../services/alertify.service';
 
 @Component({
   selector: 'app-nav-bar',
@@ -7,9 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavBarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router,
+              private alertifyService: AlertifyService) { }
 
   ngOnInit() {
+  }
+
+  loggedIn(){
+    return localStorage.getItem('token');
+  }
+
+  onLogout(){
+    localStorage.removeItem('token');
+    this.alertifyService.successAlert('Sign out has succeeded');
   }
 
 }
--- src/app/user/user-login/user-login.component.ts
+++ src/app/user/user-login/user-login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AlertifyService } from 'src/app/services/alertify.service';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-user-login',
@@ -7,9 +11,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserLoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private userService: UserService,
+              private alertifyService: AlertifyService,
+              private router: Router) { }
 
   ngOnInit() {
+  }
+
+  onLogin(loginForm: NgForm){
+    const user = this.userService.authUser(loginForm.value);
+    debugger;
+    if (user){
+      localStorage.setItem('token', user.email);
+      this.alertifyService.successAlert('Login Sucessfully');
+      this.router.navigate(['/']);
+    } else {
+      this.alertifyService.errorAlert('Login Failed, Check your email or password');
+    }
   }
 
 }
