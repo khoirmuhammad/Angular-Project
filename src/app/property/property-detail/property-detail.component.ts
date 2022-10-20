5aedd8b5f6144759262481e368eeb0b5f76dda41
Muhammad Khoirudin <mk.muhammadkhoirudin@gmail.com>
2022-10-20 13:27:21 +0700

    Image slider preview

--- src/app/property/property-detail/property-detail.component.ts
+++ src/app/property/property-detail/property-detail.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { PropertyService } from 'src/app/services/property.service';
 import { Property } from 'src/app/models/property';
+import {NgxGalleryOptions} from '@kolkov/ngx-gallery';
+import {NgxGalleryImage} from '@kolkov/ngx-gallery';
+import {NgxGalleryAnimation} from '@kolkov/ngx-gallery';
 
 @Component({
   selector: 'app-property-detail',
@@ -12,6 +15,9 @@ export class PropertyDetailComponent implements OnInit {
   public propertyId: number;
   property = new Property();
 
+  galleryOptions: NgxGalleryOptions[];
+  galleryImages: NgxGalleryImage[];
+
   constructor(private route: ActivatedRoute,
               private router: Router,
               private propertyService: PropertyService) { }
@@ -19,6 +25,7 @@ export class PropertyDetailComponent implements OnInit {
   ngOnInit() {
     this.propertyId = +this.route.snapshot.params['id'];
     this.onLoad();
+    this.onLoadImages();
   }
 
   onLoad() {
@@ -32,15 +39,56 @@ export class PropertyDetailComponent implements OnInit {
             if (data != null) {
               this.property = data;
             }
+          },
+          error => {
+            debugger
+            this.router.navigate(['/'])
           }
         );
       }
     );
   }
 
-  onClickNextButton() {
-    this.propertyId += 1;
-    this.router.navigate(['detail-property', this.propertyId])
+
+  onLoadImages(){
+    this.galleryOptions = [
+      {
+        width: '100%',
+        height: '465px',
+        thumbnailsColumns: 4,
+        imageAnimation: NgxGalleryAnimation.Slide,
+        preview: true
+      }
+    ];
+
+    this.galleryImages = [
+      {
+        small: 'assets/images/img-1.png',
+        medium: 'assets/images/img-1.png',
+        big: 'assets/images/img-1.png'
+      },
+      {
+        small: 'assets/images/img-2.png',
+        medium: 'assets/images/img-2.png',
+        big: 'assets/images/img-2.png'
+      },
+      {
+        small: 'assets/images/img-3.png',
+        medium: 'assets/images/img-3.png',
+        big: 'assets/images/img-3.png'
+      },
+      {
+        small: 'assets/images/img-4.png',
+        medium: 'assets/images/img-4.png',
+        big: 'assets/images/img-4.png'
+      },
+      {
+        small: 'assets/images/img-5.png',
+        medium: 'assets/images/img-5.png',
+        big: 'assets/images/img-5.png'
+      }
+    ];
   }
+
 
 }
--- src/app/services/property.service.ts
+++ src/app/services/property.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { map, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 import { Property } from '../models/property';
 
 @Injectable({
